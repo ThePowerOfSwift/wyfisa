ee49b5face4ac893de7fd70bc5ebb9144b22f31f
Loaf Box <loafboxit@gmail.com>
2016-11-30 14:16:18 -0500

    save images as attachments

--- WYFISA/CBStorage.swift
+++ WYFISA/CBStorage.swift
@@ -41,6 +41,9 @@ class CBStorage {
                 while let row = result.nextRow() {
                     let doc = row.value
                     if let verse = VerseInfo.DocPropertiesToObj(doc) {
+                        verse.image = self.getImageAttachment(verse.createdAt, named: "original.jpg")
+                        verse.overlayImage = self.getImageAttachment(verse.createdAt, named: "overlay.jpg")
+                        verse.accessoryImage = self.getImageAttachment(verse.createdAt, named: "accessory.jpg")
                         recentVerses.append(verse)
                     }
                 }
@@ -64,8 +67,53 @@ class CBStorage {
             let properties =  verse.toDocProperties()
             do {
                 try doc.putProperties(properties)
+                if verse.category == .Image {
+                    self.attachImage(doc, image: verse.image, named: "original.jpg")
+                    self.attachImage(doc, image: verse.overlayImage, named: "overlay.jpg")
+                    self.attachImage(doc, image: verse.accessoryImage, named: "accessory.jpg")
+                }
             } catch {
                 print("save verse failed")
+            }
+        }
+    }
+    
+    func getAttachment(key: String, named: String) -> NSData? {
+        
+        var data: NSData?
+        if let db = self.db {
+            if let doc = db.documentWithID(key) {
+                if let rev = doc.currentRevision {
+                    let att = rev.attachmentNamed(named)
+                    data = att?.content
+                }
+            }
+        }
+        
+        return data
+    }
+    
+    func getImageAttachment(key: String, named: String) -> UIImage? {
+        if let imgData =  self.getAttachment(key, named: named) {
+            return UIImage(data: imgData)
+        }
+        return nil
+    }
+    
+    func attachImage(doc: CBLDocument, image: UIImage?, named: String)  {
+        
+        // attach image
+        if let rev = doc.currentRevision {
+            let newRev = rev.createRevision()
+            
+            if let img = image {
+                let imageData = UIImageJPEGRepresentation(img, 0.75)
+                newRev.setAttachmentNamed(named, withContentType: "image/jpeg", content: imageData)
+                do {
+                    try newRev.save()
+                } catch {
+                    print("attach image failed")
+                }
             }
         }
     }
